ed80a22373a90f531040288fb0ac396cee03ae44
Sidney.E.S.S.Jr <sidney.E.S.S.Jr@gmail.com>
2021-02-24 13:20:48 -0300

    Estado e Cidade

--- backend/src/main/java/br/com/backendspring/backend/config/Instanciando.java
+++ backend/src/main/java/br/com/backendspring/backend/config/Instanciando.java
@@ -7,8 +7,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.backendspring.backend.domains.Categoria;
+import br.com.backendspring.backend.domains.Cidade;
+import br.com.backendspring.backend.domains.Estado;
 import br.com.backendspring.backend.domains.Produto;
 import br.com.backendspring.backend.repositories.CategoriaRepository;
+import br.com.backendspring.backend.repositories.CidadeRepository;
+import br.com.backendspring.backend.repositories.EstadoRepository;
 import br.com.backendspring.backend.repositories.ProdutoRepository;
 
 @Configuration
@@ -20,11 +24,19 @@ public class Instanciando implements CommandLineRunner {
     @Autowired
     private ProdutoRepository pRepository;
 
+    @Autowired
+    private EstadoRepository eRepository;
+
+    @Autowired
+    private CidadeRepository cidRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         cRepository.deleteAll();
         pRepository.deleteAll();
+        cidRepository.deleteAll();
+        eRepository.deleteAll();
 
         Categoria cat1 = new Categoria(null, "Informática");
         Categoria cat2 = new Categoria(null, "Escritório");
@@ -33,13 +45,19 @@ public class Instanciando implements CommandLineRunner {
         Produto p2 = new Produto(null, "Impressora", 800.00);
         Produto p3 = new Produto(null, "Mouse", 80.00);
 
-        // cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-        // cat2.getProdutos().add(p2);
-
         p1.getCategorias().add(cat1);
         p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
         p3.getCategorias().add(cat1);
 
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+        eRepository.saveAll(Arrays.asList(est1, est2));
+        cidRepository.saveAll(Arrays.asList(c1, c2, c3));
         cRepository.saveAll(Arrays.asList(cat1, cat2));
         pRepository.saveAll(Arrays.asList(p1, p2, p3));
 
