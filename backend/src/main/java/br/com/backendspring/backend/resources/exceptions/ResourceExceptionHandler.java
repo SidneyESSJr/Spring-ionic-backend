2c8614343ed30690f98d4d2d7e949646e110ddd0
Sidney.E.S.S.Jr <sidney.E.S.S.Jr@gmail.com>
2021-03-16 17:52:15 -0300

    ValidationError - Mensagem personalizada

--- backend/src/main/java/br/com/backendspring/backend/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/br/com/backendspring/backend/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package br.com.backendspring.backend.resources.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,16 +14,23 @@ public class ResourceExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandartError> objectNotFound(ObjectNotFoundException e) {
-        StandartError standartError = new StandartError(HttpStatus.NOT_FOUND.value(), e.getMessage(),
-                System.currentTimeMillis());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standartError);
+        StandartError err = new StandartError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 
     @ExceptionHandler(DataIntegrityException.class)
     public ResponseEntity<StandartError> DataIntegrity(DataIntegrityException e) {
-        StandartError standartError = new StandartError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+        StandartError err = new StandartError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
                 System.currentTimeMillis());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standartError);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandartError> validation(MethodArgumentNotValidException e) {
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação de campo",
+                System.currentTimeMillis());
+        e.getBindingResult().getFieldErrors().forEach(field -> err.setErros(field.getField(), field.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
 }
